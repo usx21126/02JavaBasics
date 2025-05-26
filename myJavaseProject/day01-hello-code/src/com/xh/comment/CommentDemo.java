70f9d7f98d5df6dc057e682b3ebc15edfa9a4cd3
2746150991 <2746150991@qq.com>
2025-05-26 16:46:46 +0800

    first commit

---
+++ myJavaseProject/day01-hello-code/src/com/xh/comment/CommentDemo.java
@@ -0,0 +1,25 @@
+package com.xh.comment;
+
+/**
+ * 哈哈哈哈
+ * 哈哈哈
+ * 哈哈
+ * 哈
+ */
+public class CommentDemo {
+    public static void main(String[] args) {
+        //1.单行注释
+        System.out.println("轻舟已过万重山");
+        /*
+          2.多行注释
+          哈哈哈
+          哈哈
+          哈
+         */
+
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+
+    }
+}

be5afe8d52afda9dd226bda655d368c07ab6624d
2746150991 <2746150991@qq.com>
2025-05-26 17:06:49 +0800

    day02 commit

---
+++ myJavaseProject/day02-basic-code/src/scanner/ScannerDemo1.java
@@ -0,0 +1,16 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入年龄：");
+        int age = sc.nextInt();
+        System.out.println("您今年："+ age + "岁");
+
+        System.out.println("请输入名字");
+        String name = sc.next();
+        System.out.println("欢迎您" + name);
+    }
+}
